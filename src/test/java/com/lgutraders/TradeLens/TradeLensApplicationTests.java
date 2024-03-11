2deff6f89a17ec32aead76238ee18b376e1da6ca
unknown <eminoviciandrei777@yahoo.com>
2024-03-11 17:17:44 +0200

    Project Initialization

---
+++ src/test/java/com/lgutraders/TradeLens/TradeLensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgutraders.TradeLens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeLensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
